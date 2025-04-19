7cb450a9c428cf63f01623f06207367e9046d9ba
Kyle <kyle11493@gmail.com>
2025-04-19 05:15:22 -0400

    Gruvbox-Material: Removed transparency

--- lua/kickstart/plugins/gruvbox-material.lua
+++ lua/kickstart/plugins/gruvbox-material.lua
@@ -3,9 +3,12 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    -- vim.g.gruvbox_material_background = 'hard'
+    vim.g.gruvbox_material_background = 'hard'
+    vim.g.gruvbox_material_foreground = 'mix'
     vim.g.gruvbox_material_enable_bold = 1
-    vim.g.gruvbox_material_transparent_background = 2
+    vim.g.gruvbox_material_menu_selection_background = 'yellow'
+    vim.g.gruvbox_material_ui_contrast = 'high'
+    -- vim.g.gruvbox_material_transparent_background = 2
     vim.cmd.colorscheme 'gruvbox-material'
   end,
 }
